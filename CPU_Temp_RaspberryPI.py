4f68d3c3697614d4063c4c40743338f9f74e0ab3
Fajar Muhammad Noor Rozaqi / ザキ <46889787+fajarmnrozaki@users.noreply.github.com>
2022-07-30 20:28:53 +0700

    Update CPU_Temp_RaspberryPI.py with CSV & RDS MySql

--- CPU_Temp_RaspberryPI.py
+++ CPU_Temp_RaspberryPI.py
@@ -33,23 +33,23 @@ while True:
         #Uncomment this below script, if you also use the RDS (Relational database service) for MySQL Database
         #Change the [host],[user],[password], and [db] with your own]
         
-#         db = pymysql.connect(host='*****',
-#                      user='****',
-#                      password='****',
-#                      db='*****',
-#                      charset='utf8',
-#                      cursorclass=pymysql.cursors.DictCursor)
-#         cur = db.cursor()
+        db = pymysql.connect(host='*****',
+                     user='****',
+                     password='****',
+                     db='*****',
+                     charset='utf8',
+                     cursorclass=pymysql.cursors.DictCursor)
+        cur = db.cursor()
         
         try:
               #Uncomment this below script, if you also use the RDS (Relational database service) for MySQL Database
               #Change with your own name version of querry
           
-#             add_c1 ="INSERT INTO `cpu_temperature`(temperature,time,raspi_id) VALUES(%s,%s,%s)"
-#             cur.execute(add_c1,(cpu.temperature,
-#                                 timer.strftime("%Y-%m-%d %H:%M:%S"),
-#                                 int(1)))
-#             db.commit()
+            add_c1 ="INSERT INTO `cpu_temperature`(temperature,time,raspi_id) VALUES(%s,%s,%s)"
+            cur.execute(add_c1,(cpu.temperature,
+                                timer.strftime("%Y-%m-%d %H:%M:%S"),
+                                int(1)))
+            db.commit()
             
             time.sleep(2)
             
@@ -59,52 +59,52 @@ while True:
                 #Uncomment this below script, if you also use the RDS (Relational database service) for MySQL Database
                 #Change the [host],[user],[password], and [db] with your own]
               
-#                 db = pymysql.connect(host='****',
-#                      user='****',
-#                      password='****',
-#                      db='****',
-#                      charset='utf8',
-#                      cursorclass=pymysql.cursors.DictCursor)
-#                 db.cursor()
+                db = pymysql.connect(host='****',
+                     user='****',
+                     password='****',
+                     db='****',
+                     charset='utf8',
+                     cursorclass=pymysql.cursors.DictCursor)
+                db.cursor()
 
                 #Uncomment this below script, if you also use the RDS (Relational database service) for MySQL Database
                 #Change with your own name version of querry
     
-#                 with db.cursor() as cursor:
-#                     sql = "SELECT (temperature),(time),(raspi_id) FROM `cpu_temperature`"
-#                     cursor.execute(sql)
-#                     result = cursor.fetchall()
-#                     print(result)
+                with db.cursor() as cursor:
+                    sql = "SELECT (temperature),(time),(raspi_id) FROM `cpu_temperature`"
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                    print(result)
                 
                  #Uncomment this below script, if you also use want to converting the file into CSV format
                  #Change with your own name version of querry
       
-#                 Temperature =[]
-#                 Time =[]
-#                 Raspi_id =[]
+                Temperature =[]
+                Time =[]
+                Raspi_id =[]
                  
                 #Converting into CSV File
-#                 filename = "/home/pi/temperaturelog.csv"
-#                 with open(filename, mode='w', newline='') as file:
-#                     a = csv.writer(file, delimiter =',')
-#                     a.writerow(['Temperature','Time','Raspi_id'])
-#                     for i in range(0, len(result)):
-#                         a.writerow(result[i].values())
-#                         Temperature.append(result[i]['temperature'])
-#                         Time.append(result[i]['time'])
-#                         Raspi_id.append(result[i]['raspi_id'])
+                filename = "/home/pi/temperaturelog.csv"
+                with open(filename, mode='w', newline='') as file:
+                    a = csv.writer(file, delimiter =',')
+                    a.writerow(['Temperature','Time','Raspi_id'])
+                    for i in range(0, len(result)):
+                        a.writerow(result[i].values())
+                        Temperature.append(result[i]['temperature'])
+                        Time.append(result[i]['time'])
+                        Raspi_id.append(result[i]['raspi_id'])
                 
-#                 print("CSV is converted")
-#                 print("")
+                print("CSV is converted")
+                print("")
                 time.sleep(0)
   
             except:
-#                 print("CSV is not converted")
-#                 print("")
+                print("CSV is not converted")
+                print("")
                 time.sleep(0)
         except:
-#             db.rollback()
-#             print("Database is not connected")
+            db.rollback()
+            print("Database is not connected")
             
         time.sleep(0)
         
